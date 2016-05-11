26d7b7adb8aa265f67214170529992d787dcd17f
sojuyang <susan.yang301@gmail.com>
2016-05-10 23:45:16 -0400

    phase2

--- js/init.js
+++ js/init.js
@@ -30,5 +30,10 @@ $(function() {
  });
 });
 
+ $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
   }); // end of document ready
 })(jQuery); // end of jQuery name space

f0022488394394d640424ad1be08041e294241ef
sojuyang <susan.yang301@gmail.com>
2016-05-11 13:30:39 -0400

    phase3

--- js/init.js
+++ js/init.js
@@ -35,5 +35,10 @@ $(function() {
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
   });
+
+     $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space

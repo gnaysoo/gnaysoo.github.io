b5cb8c2082fb7546bcddcc0b8fd879d610fd46eb
sojuyang <susan.yang301@gmail.com>
2016-05-20 16:37:44 -0400

    work1done

--- js/init.js
+++ js/init.js
@@ -42,5 +42,10 @@ $(function() {
       $('.parallax').parallax();
     });
 
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
